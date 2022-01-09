ac2de0048bef99ebbf9aee55fa31d6b045893f2c
Davide Messori <davide.messori@outlook.com>
2022-01-09 22:43:11 +0100

    Fix product page light theme

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -4,10 +4,12 @@ import SubNav from "../../components/SubNav";
 import TopNav from "../../components/TopNav";
 import productAtom from "../../recoil/atom"
 import Footer from "../../components/Footer"
+import { useSession } from "next-auth/react"
 
 function ProductPage() {
     const [product, setProduct] = useRecoilState(productAtom);
     const [data, setData] = useState({})
+    const { data: session } = useSession();
 
     console.log(data)
     console.log(Object.entries(data))
@@ -24,24 +26,22 @@ function ProductPage() {
                         <div className="flex-col sm:flex-row flex w-full sm:justify-evenly p-8">
                             <div className="flex h-fit">
 
-                                <img className="h-[70%] w-[40%]" src={product?.imageUrl}></img>
+                                <img className="h-[70%] w-[40%] shadow-lg" src={product?.imageUrl}></img>
                                 <p className="text-left w-full dark:text-white pl-8 font-semibold text-xl md:text-2xl">{product?.title}</p>
                             </div>
-                            <div className="dark:bg-">
-                                <div className="bg-slate-100 dark:bg-slate-800 rounded-lg p-6 shadow-lg mx-4 my-8 sm:my-0">
-                                    <p className="dark:text-white my-2 h-min">
-                                        {product.price}
-                                    </p>
-                                    <p className="dark:text-white">
-                                        {product.isPrimeEligible === "1" ? "Prime" : "Non Prime"}
-                                    </p>
-                                    <div className="w-max bg-slate-300 p-4 rounded-xl cursor-pointer mt-4">
-                                        Add to cart
-                                </div>
+                            <div className="bg-slate-100 dark:bg-slate-800 rounded-lg p-6 shadow-lg mx-4 my-8 h-min sm:my-0">
+                                <p className="dark:text-white my-2 h-min">
+                                    {product.price}
+                                </p>
+                                <p className="dark:text-white">
+                                    {product.isPrimeEligible === "1" ? "Prime" : "Non Prime"}
+                                </p>
+                                <div className={"w-max bg-slate-300 p-4 rounded-xl mt-4 shadow-lg cursor-" + (session ? "pointer" : "not-allowed")}>
+                                    Add to cart
                                 </div>
                             </div>
                         </div>
-                        <div className="text-white mx-12 font-semibold">
+                        <div className="dark:text-white mx-12 font-semibold">
                             <p> Rating: {product.rating} ⭐ </p>
                             <p> {product.totalReviews} reviews</p>
                         </div>
